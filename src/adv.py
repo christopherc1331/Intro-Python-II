766ec5f3c6efa5ad18999b7dc2c236da138ca2d0
Chris Cruz <christopherc1331@gmail.com>
2020-05-14 20:14:09 -0700

    Added item_list and print method to room class

--- src/adv.py
+++ src/adv.py
@@ -1,31 +1,50 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 rooms = {
-    "outside": Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    "outside": Room(
+        "Outside Cave Entrance",
+        "North of you, the cave mount beckons",
+        [
+            Item("dirt", "Some dirt on the ground"),
+            Item("jar", "Can be used to hold things"),
+        ],
+    ),
     "foyer": Room(
         "Foyer",
         """Dim light filters in from the south. Dusty
 passages run north and east.""",
+        [Item("glass", "Thirsty?"), Item("light_bulb", "A viable light source"),],
     ),
     "overlook": Room(
         "Grand Overlook",
         """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""",
+        [
+            Item("frog", "Rrrribbbit!"),
+            Item("coffee", "It's still warm..."),
+            Item("pepper_spray", "The ultimate stay away juice"),
+        ],
     ),
     "narrow": Room(
         "Narrow Passage",
         """The narrow passage bends here from west
 to north. The smell of gold permeates the air.""",
+        [Item("torch", "Should probably hang on to this")],
     ),
     "treasure": Room(
         "Treasure Chamber",
         """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""",
+        [
+            Item("", "Should probably hang on to this"),
+            Item("notepad", "I think I can see some writing on here"),
+        ],
     ),
 }
 
@@ -40,10 +59,6 @@ rooms["overlook"].s_to = rooms["foyer"]
 rooms["narrow"].w_to = rooms["foyer"]
 rooms["narrow"].n_to = rooms["treasure"]
 rooms["treasure"].s_to = rooms["narrow"]
-
-
-key_list = list(rooms.keys())
-val_list = list(rooms.values())
 
 
 #
@@ -72,6 +87,8 @@ while entry != "q":
     print("")
     print("=========================")
     print(player1)
+    print("")
+    player1.current_room.print_items_in_room()
     print("=========================")
     print("")
     entry = input("Enter a direction. Enter q to quit: ")
--- src/room.py
+++ src/room.py
@@ -1,11 +1,21 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+from item import Item
+
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, item_list=[]):
         self.name = name
         self.description = description
+        self.item_list = item_list
 
     def __str__(self):
         return f"{self.name}: {self.description}"
+
+    def add_item_to_room(self, name, description):
+        self.item_list.append(Item(name, description))
+
+    def print_items_in_room(self):
+        for index, item in enumerate(self.item_list):
+            print(f"{index + 1}: {item}")
